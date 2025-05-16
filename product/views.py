7b7f8579a6fa2cb613f431b4d88d24da01d51e3a
theertha09 <161118217+theertha09@users.noreply.github.com>
2025-05-16 17:09:58 +0530

    .

--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from .models import Resort, State
 from .serializers import ResortSerializer, StateSerializer
 
@@ -6,15 +8,21 @@ from .serializers import ResortSerializer, StateSerializer
 class StateListCreateAPIView(generics.ListCreateAPIView):
     queryset = State.objects.all()
     serializer_class = StateSerializer
+    authentication_classes = []  # No authentication
+    permission_classes = [AllowAny]  # Allow all users
 
 class StateRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = State.objects.all()
     serializer_class = StateSerializer
     lookup_field = 'id'
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
 # RESORT VIEWS
 class ResortListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ResortSerializer
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         queryset = Resort.objects.all()
@@ -27,3 +35,5 @@ class ResortRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Resort.objects.all()
     serializer_class = ResortSerializer
     lookup_field = 'id'
+    authentication_classes = []
+    permission_classes = [AllowAny]
