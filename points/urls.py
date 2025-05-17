f8a3dbbccb09ab01bc844e2ec03d9deb660d69cb
theertha09 <161118217+theertha09@users.noreply.github.com>
2025-05-17 11:33:02 +0530

    .

--- points/urls.py
+++ points/urls.py
@@ -4,4 +4,4 @@ from .views import pointsListCreateAPIView,pointsRetrieveUpdateDestroyAPIView
 urlpatterns = [
     path('points/', pointsListCreateAPIView.as_view(), name='points-list'),
     path('points/<int:id>/', pointsRetrieveUpdateDestroyAPIView.as_view(), name='points-detail'),
-]
+]0
